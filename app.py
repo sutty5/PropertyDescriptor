7612513a2edc3c93a64221abde257676983f7921
Jordan Sutton <sutty50@gmail.com>
2023-05-24 20:28:58 +0100

    updated

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def handle_start(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='192.168.86.39', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
